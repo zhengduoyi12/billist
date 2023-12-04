6333341f0693da4bd33fcd7ba03bf9bc78973099
alex zheng <zhengduoyi12@gmail.com>
2023-12-04 14:42:56 +1100

    router init

--- src/index.js
+++ src/index.js
@@ -1,8 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import App from './App';
 
+import router from './router'
+import { RouterProvider } from 'react-router-dom';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-    <App />
+  <RouterProvider router = { router}> </RouterProvider>
 );
---
+++ src/pages/Layout/index.js
@@ -0,0 +1,8 @@
+import { Outlet } from "react-router-dom"
+const Layout = ()=>{
+    return <div>Layout
+        <Outlet/>
+    </div>
+}
+
+export default Layout
